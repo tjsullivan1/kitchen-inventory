c12529b6b07b609cad496851e40d7f6da81a48d4
Tim Sullivan <timothyj.sullivan1@github.com>
2021-12-02 21:38:25 -0600

    added secrets for access key and connection string

--- infra/main.tf
+++ infra/main.tf
@@ -53,3 +53,79 @@ resource "azurerm_storage_account" "ki" {
 
   tags = var.tags
 }
+
+resource "azurerm_key_vault" "ki" {
+  name                        = "akv-${var.disambiguation}-${random_string.suffix.result}"
+  location                    = azurerm_resource_group.ki.location
+  resource_group_name         = azurerm_resource_group.ki.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Backup",
+      "Create",
+      "Decrypt",
+      "Delete",
+      "Encrypt",
+      "Get",
+      "Import",
+      "List",
+      "Purge",
+      "Recover",
+      "Restore",
+      "Sign",
+      "UnwrapKey",
+      "Update",
+      "Verify",
+      "WrapKey"
+    ]
+
+    secret_permissions = [
+      "Backup",
+      "Delete",
+      "Get",
+      "List",
+      "Purge",
+      "Recover",
+      "Restore",
+      "Set"
+    ]
+
+    storage_permissions = [
+      "Backup",
+      "Delete",
+      "DeleteSAS",
+      "Get",
+      "GetSAS",
+      "List",
+      "ListSAS",
+      "Purge",
+      "Recover",
+      "RegenerateKey",
+      "Restore",
+      "Set",
+      "SetSAS",
+      "Update"
+    ]
+  }
+}
+
+resource "azurerm_key_vault_secret" "storage-account-key" {
+  name         = "${azurerm_storage_account.ki.name}-key"
+  value        = azurerm_storage_account.ki.primary_access_key
+  key_vault_id = azurerm_key_vault.ki.id
+}
+
+resource "azurerm_key_vault_secret" "storage-account-connection-string" {
+  name         = "${azurerm_storage_account.ki.name}-connection-string"
+  value        = azurerm_storage_account.ki.primary_connection_string
+  key_vault_id = azurerm_key_vault.ki.id
+}

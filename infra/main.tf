47f116ddc8c6a19d5fde36b84b3521fcb4e0b50d
Tim Sullivan <timothyj.sullivan1@gmail.com>
2021-12-03 14:21:44 -0600

    updated terraform dependencies for the acess policies for KV

--- infra/main.tf
+++ infra/main.tf
@@ -183,11 +183,13 @@ resource "azurerm_key_vault_secret" "storage-account-key" {
   name         = "${azurerm_storage_account.ki.name}-key"
   value        = azurerm_storage_account.ki.primary_access_key
   key_vault_id = azurerm_key_vault.ki.id
+  depends_on = [ azurerm_key_vault_access_policy.spn]
 }
 
 resource "azurerm_key_vault_secret" "storage-account-connection-string" {
   name         = "${azurerm_storage_account.ki.name}-connection-string"
   value        = azurerm_storage_account.ki.primary_connection_string
   key_vault_id = azurerm_key_vault.ki.id
+  depends_on = [ azurerm_key_vault_access_policy.spn]
 }
 
